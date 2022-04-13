9efde0315438a02b851390fc96076720f334ec39
beygee <doug0476@naver.com>
2022-04-13 12:23:38 +0900

    feat: add tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ export const HomePage = () => {
   return (
     <div className="App">
       <Button>Hello Homse!</Button>
+      <span className="font-bold text-red-500">asasdd</span>
     </div>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  corePlugins: {
+    preflight: false,
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
